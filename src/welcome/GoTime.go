2d2f8e37b5a36b6f40dded787ed1f6cdff7f7f05
Caesar <caesarjuming@gmail.com>
2016-02-15 18:11:09 +0800

    add

---
+++ src/welcome/GoTime.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	p := fmt.Println
+
+	now := time.Now()
+	p(now)
+
+	then := time.Date(2016, 02, 02, 20, 18, 10, 1000, time.UTC)
+	p(then)
+
+	p(then.Year())
+	p(then.Month())
+	p(then.Day())
+	p(then.Hour())
+	p(then.Minute())
+	p(then.Second())
+	p(then.Nanosecond())
+	p(then.Location())
+	p(then.Weekday())
+	p(then.Before(now))
+	p(then.After(now))
+	p(then.Equal(now))
+
+	diff := now.Sub(then)
+	p(diff)
+
+	p(diff.Hours())
+	p(diff.Minutes())
+	p(diff.Seconds())
+	p(diff.Nanoseconds())
+
+	p(then.Add(diff))
+	p(then.Add(-diff))
+}
